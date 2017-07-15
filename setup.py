a49dd3241126733d6a23b4fdc40304b9b8fb2ba4
msuhanov <fuf@localhost>
2017-07-15 18:30:33 +0300

    Minor change: add the yarp-mount script to the setup.py (beta).

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	version = __version__,
 	license = 'GPLv3',
 	packages = [ 'yarp' ],
-	scripts = [ 'yarp-carver', 'yarp-print', 'yarp-timeline' ],
+	scripts = [ 'yarp-carver', 'yarp-print', 'yarp-timeline', 'yarp-mount' ],
 	description = 'Yet another registry parser',
 	author = 'Maxim Suhanov',
 	author_email = 'no.spam.c@mail.ru',
