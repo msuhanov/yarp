351c79a742a2570987f9b2468ddec7ef2ed3e0f9
msuhanov <fuf@localhost>
2017-10-04 01:03:37 +0300

    Minor update (beta).

--- yarp/__init__.py
+++ yarp/__init__.py
@@ -1,5 +1,5 @@
 # yarp: yet another registry parser
 # (c) Maxim Suhanov
 
-__version__ = '1.0.0-beta35'
+__version__ = '1.0.0-beta36'
 __all__ = [ 'Registry', 'RegistryFile', 'RegistryRecords', 'RegistryRecover', 'RegistryCarve', 'RegistryHelpers' ]
